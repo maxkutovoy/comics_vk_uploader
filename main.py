e91ac37cfed81cf096308b5abb25b9076d24675c
Max Kutovoy <maxkutovoy@yandex.ru>
2021-11-03 19:44:52 +0600

    Написал функцию для получения текущего комикса

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+from pprint import pprint
+from pathlib import Path
 
-# Press F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+
+import os
+from urllib.parse import unquote, urlsplit
+
+from tldextract import extract
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+def define_filename_prefix(url):
+    extracted_url = extract(url)
+    return extracted_url.domain
 
 
-# Press the green button in the gutter to run the script.
+def pars_filename(url):
+    parsed_url = urlsplit(url)
+    filename = os.path.split(unquote(parsed_url.path))[1]
+    return filename
+
+
+def fetch_comic_book():
+    url = "https://xkcd.com/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    current_comic = response.json()
+    pprint(current_comic)
+
+    directory = 'files'
+    image_url = current_comic['img']
+    filename = pars_filename(image_url)
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    file_path = f"{directory}/{filename}"
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+
+def main():
+    fetch_comic_book()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
